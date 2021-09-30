ab1363d2ff3b90c5d1f4a05d2bced2645ffe03f4
mfleck <michael.j.fleck@gmail.com>
2021-09-30 16:11:01 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Write your solution in this file!
+
+const employee = {};
+employee.name = "Michael";
+employee.streetAddress = "Gillette"
+
+//hint: use spread operator
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return{
+        ...obj,
+        [key]: value,
+    };
+}
+
+const updemployee = updateEmployeeWithKeyAndValue (
+    employee,
+    "Sam",
+    "11 Broadway"
+);
+
+function destructivelyUpdateEmployeeWithKeyAndValue (obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+const desemployee = destructivelyUpdateEmployeeWithKeyAndValue (
+    employee,
+    "Sam",
+    "12 Broadway"
+);
+
+//hint: use spread operator
+function deleteFromEmployeeByKey(obj, key, value) {
+    const newObj = { ...obj };
+
+    newObj[key] = value;
+
+    return newObj;
+}
+
+const des2employee = deleteFromEmployeeByKey(
+    employee,
+    "Sam"
+);
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) { 
+    delete employee[key];
+    return employee;
+};
+
+const ddfemployee = destructivelyDeleteFromEmployeeByKey(
+    employee,
+    "12 Broadway"
+)
+
+
+
+
